2b4e3e9588d51319f703721ca25a280684e73441
Sergey <shiryaev.sut@gmail.com>
2017-03-11 22:21:46 +0300

    kokoko

---
+++ index.php
@@ -0,0 +1,7 @@
+﻿<HTML>
+<FORM ACTION="lab5.php" METHOD="POST">
+Введите код цвета <INPUT TYPE="text" name="rgb"><BR>
+Выберите цвет <SELECT name="color"> <option> Красный </option> <option> Зеленый </option> <option> Синий </option> </SELECT>
+<INPUT TYPE="submit" VALUE="ВВОД">
+</FORM>
+</HTML>
